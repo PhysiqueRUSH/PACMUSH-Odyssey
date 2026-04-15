0dd7d959849fc4b5334df8386a822983789fd3be
PhysiqueRUSH <physiquerush@gmail.com>
2026-04-15 23:31:11 +0200

    Update cache name to version 1.7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker pour PACMUSH ODYSSEY
-const CACHE_NAME = 'pacmush-odyssey-v1.6';
+const CACHE_NAME = 'pacmush-odyssey-v1.7';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
